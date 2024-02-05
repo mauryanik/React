5ef13fdd0918b20dbe92fda3bc0a0dfdc6ccbfa8
Nikesh Maurya <nikeshinknit@gmail.com>
2024-02-06 01:26:41 +0530

    Second Commit

--- App.js
+++ App.js
@@ -5,9 +5,12 @@
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading); //render method converts the heading object to h1 tag.
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const parent = React.createElement("div",{id:"parent"},[
 React.createElement("div",{id:"child1"},[
-React.createElement("h1",{},"I am h1 tag"),
+React.createElement("h1",{},"I am h1 tag Namaste React"),
 React.createElement("h2",{},"I am h2 tag")]),
 React.createElement("div",{id:"child2"},[
 React.createElement("h1",{},"I am h1 tag"),
